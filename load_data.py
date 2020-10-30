b0b95071b01058a1e4d962e32101cbed19f501d3
Ruben Miranda <ruben.miranda.f@gmail.com>
2020-10-30 14:59:29 -0300

    Add files via upload

---
+++ load_data.py
@@ -0,0 +1,16 @@
+from six.moves import urllib
+#from sklearn.datasets import fetch_mldata
+from sklearn.datasets import fetch_openml
+#mnist = fetch_openml('mnist_784')
+
+def load_mnist():
+    mnist_path = "./mnist-original.mat"
+    from scipy.io import loadmat
+    mnist_raw = loadmat(mnist_path)
+    mnist = {
+        "data": mnist_raw["data"].T,
+        "target": mnist_raw["label"][0],
+        "COL_NAMES": ["label", "data"],
+        "DESCR": "mldata.org dataset: mnist-original",
+    }
+    return mnist
